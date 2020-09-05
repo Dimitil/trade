876a0872618a530566cfeef5a6a645d2c32e15fa
Dimitil <boikovdmitrii@gmail.com>
2020-09-05 13:57:50 +0300

    operator << overloaded, edit updateSnapshot

--- main.cpp
+++ main.cpp
@@ -1,81 +1,120 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <cstdlib> // для использования функции exit()
-#include <vector>
 #include <sstream>
+#include <map>
+
+void formatStr(std::string &strInput){
+    const std::string& delims = "[]\",:";
+    for(auto t_iter = strInput.begin(); t_iter != strInput.end(); ++t_iter){
+        if(delims.find(*t_iter) != std::string::npos){
+            *t_iter = ' ';
+        }
+    }
+}
+
+bool haveData(std::istringstream &ist){
+    char ch=' ';
+    ist.get(ch);
+    while(isspace(ch)){
+       ist.get(ch);
+    }
+    ist.putback(ch);
+    return isdigit(ch);
+}
+
+
+class Snapshot{
+    long int m_curTime;
+    std::map <double, int> m_asks;
+    std::map <double, int> m_bids;
+
+public:
+
+    void print() const{
+        int num=1;
+        std::cout<<'\n'<<m_curTime<<"\n\n";
+        for(auto it=m_asks.begin(); it!=m_asks.end(); it++)
+        {
+            std::cout<<num++<<')';
+            std::cout<<it->first<<'\t'<<it->second;
+            std::cout<<'\n';
+        }
+        num=1;
+        std::cout<<'\n';
+        for(auto it=m_bids.begin(); it!=m_bids.end(); it++)
+        {
+            
+            std::cout<<num++<<')';
+            std::cout<<it->first<<'\t'<<it->second;
+            std::cout<<'\n';
+        }   
+    }
+
+   
+    void updateMap(std::istringstream &ist, std::map <double, int> &map)
+    {
+        int amount=-1;
+        double price = -1;
+        while(haveData(ist) && ist>>price && ist>>amount){
+            map[price]=amount;
+            auto it=map.find(price);
+            if(0==amount && it != map.end()){
+                map.erase(it);
+            }
+            amount=-1;
+            price=-1;
+        }
+    }
+
+    void updateSnapshot(std::string strInput){
+        formatStr(strInput);
+        std::istringstream ist(strInput);
+        std::string buf("");
+        while(ist>>buf) {
+            if(buf=="time"){
+                ist>>m_curTime;
+            }
+
+            if(buf=="asks"){
+                updateMap(ist, m_asks);
+            }
+
+            if(buf=="bids"){
+                updateMap(ist, m_bids);
+            }
+        }
+    }
+
+   friend std::ostream& operator<< (std::ostream &out, const Snapshot &s){
+        out<<'{'<<s.m_curTime<<"}, {"<<s.m_bids.rbegin()->first<<"}, {"
+            <<s.m_bids.rbegin()->second<<"}, {"<<s.m_asks.begin()->first<<
+            "}, {"<<s.m_asks.begin()->second<<"}\n";
+        return out;
+   } 
+    
+};
+
 
 int main()
 {
-
-    std::vector<std::pair<double, int>> asks;
-    asks.resize(20);
-    std::vector<std::pair<double, int>> bids;
-    bids.resize(20);
+    std::cout.precision(7);
     std::ifstream inf("/home/dmitry/cpp/birja/huobi_dm_depth.log");
-
     if (!inf)
     {
         std::cerr << "could not be opened for reading!" << std::endl;
         exit(1);
     }
 
+    Snapshot S{};
 
-    const std::string& delims = "[]\",:";
     std::string strInput;
-    std::getline(inf, strInput);
 
-    for(auto t_iter = strInput.begin(); t_iter != strInput.end(); ++t_iter){
-                               //Если в разделителях найден текущий символ - меняем его на пробел
-        if(delims.find(*t_iter) != std::string::npos){
-            *t_iter = ' ';
-        }
+    while(std::getline(inf, strInput)){
+        S.updateSnapshot(strInput);
+        std::cout<<S;
     }
-    std::istringstream ist(strInput);
-    std::string buf;
-    double price=0;
-    int amount=0;
-    int curTime=0;
-    while(ist) {
-        ist>>buf;
-        //std::cout<<buf<<'\n';
-        if(buf=="time"){
-            ist>>curTime;
-        }
-
-        }
-        if(buf=="asks")
-        {
-            for(int i=0; i<20; i++){
-            ist>>price;
-            ist>>amount;
-            asks[i]=std::make_pair(price, amount);
-            }
-        }
-
-        if(buf=="bids"){
-            for(int i=0; i<20; i++){
-            ist>>price;
-            ist>>amount;
-            bids[i]=std::make_pair(price, amount);
-            }
-        }
-
-
-
-    for(int i=0; i<20; i++)
-    {
-        std::cout<<asks[i].first<<'\t'<<asks[i].second;
-        std::cout<<'\n';
-    }
-
-    std::cout<<'\n';
-
-    for(int i=0; i<20; i++)
-    {
-        std::cout<<bids[i].first<<'\t'<<bids[i].second;
-        std::cout<<'\n';
-    }
-
+    inf.close();
+    
     return 0;
 }
