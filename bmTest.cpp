c89ee8a662404bf770f526206aa1d040f3523135
Dimitil <boikovdmitrii@gmail.com>
2020-09-08 19:49:00 +0300

    more fix for BM

--- bmTest.cpp
+++ bmTest.cpp
@@ -5,75 +5,63 @@
 #include <iostream>
 #include <list>
 
-void updateMap( std::ifstream &ifs, std::map <double, int> &map)
+
+const size_t ar_size = 500; //less or equal 153k
+
+void updateMap( double *ar_price, int *ar_amount, std::map <double, int> &map)
 {
-        while(ifs){
-            int amount = -1;
-            double price = -1;
-            ifs >> price >> amount;
-            if(ifs.good()) {
-                if( 0 == amount ){
-                    auto it = map.find(price);
-                    if( it != map.end() ){
-                        map.erase(it);
-                        continue;
-                    }
+       for(int i=0; i<ar_size; i++){ 
+            if( 0 == ar_amount[i] ){
+                auto it = map.find(ar_price[i]);
+                if( it != map.end() ){
+                    map.erase(it);
+                    continue;
                 }
-                if( price>0 && amount > 0 ){
-                    map[price] = amount;
-                }
+            }
+            if( ar_price[i]>0 && ar_amount[i] > 0 ){
+                map[ar_price[i]] = ar_amount[i];
             }
         }
 }
 
 
-void updateVector(std::ifstream &ifs, std::vector <std::pair<double, int>> &v){
-    while(ifs){
-        int amount = -1;
-        double price = -1;
-        ifs >> price >> amount;
-        if(ifs.good()){
-            if ( price > 0 && amount >= 0){
-               auto it = v.begin(); 
-                for(; it != v.end(); it++){
-                    if(it->first==price) break;
+void updateVector(double* ar_price, int* ar_amount, std::vector <std::pair<double, int>> &v){
+    for(int i=0; i<ar_size; i++){ 
+        if (ar_price[i] > 0 && ar_amount[i] >= 0){
+            auto it = v.begin(); 
+            for(; it != v.end(); it++){
+                if(it->first == ar_price[i]) break;
+            }
+            if( it != v.end()){
+                if(ar_amount[i] == 0){
+                   v.erase(std::remove(v.begin(), v.end(), *it), v.end());
+                   continue;
                 }
-                if( it != v.end()){
-                    if(amount == 0){
-                        v.erase(std::remove(v.begin(), v.end(), *it), v.end());
-                        continue;
-                    }
-                    it->second = amount;
-                }
-                else {
-                    v.push_back(std::make_pair(price, amount));
-                }
+                it->second = ar_amount[i];
+            }
+            else {
+                v.push_back(std::make_pair(ar_price[i], ar_amount[i]));
             }
         }
     }
 }
 
-void updateList(std::ifstream &ifs, std::list <std::pair<double, int>> &l){
-    while(ifs){
-        int amount = -1;
-        double price = -1;
-        ifs >> price >> amount;
-        if(ifs.good()){
-            if ( price > 0 && amount >= 0){
-               auto it = l.begin(); 
-                for(; it != l.end(); it++){
-                    if(it->first==price) break;
+void updateList(double* ar_price, int* ar_amount, std::list <std::pair<double, int>> &l){
+    for(int i=0; i<ar_size; i++){ 
+        if (ar_price[i] > 0 && ar_amount[i] >= 0){
+            auto it = l.begin(); 
+            for(; it != l.end(); it++){
+                if(it->first==ar_price[i]) break;
                 }
-                if( it != l.end()){
-                    if(amount == 0){
-                        l.erase(it);
-                        continue;
-                    }
-                    it->second = amount;
+            if( it != l.end()){
+                if(ar_amount[i] == 0){
+                   l.erase(it);
+                   continue;
                 }
-                else {
-                    l.push_back(std::make_pair(price, amount));
-                }
+                it->second = ar_amount[i];
+            }
+            else {
+                l.push_back(std::make_pair(ar_price[i], ar_amount[i]));
             }
         }
     }
@@ -81,34 +69,63 @@ void updateList(std::ifstream &ifs, std::list <std::pair<double, int>> &l){
 
 void BM_MapUpdate(benchmark::State& state) {
     std::map <double, int> map;
+    std::ifstream ifs("test");
+    double ar_price[ar_size];
+    int ar_amount[ar_size];
+    for(int i=0; i<ar_size; i++){
+       ifs >> ar_price[i];
+       ifs >> ar_amount[i];
+    }
+    ifs.close();
+
     for(auto _ : state) {
-        std::ifstream ifs("test");
-        updateMap(ifs, map);
-        ifs.close();
+        updateMap(ar_price, ar_amount, map);
     }
 }
-BENCHMARK(BM_MapUpdate)->Unit(benchmark::kMillisecond);
+BENCHMARK(BM_MapUpdate);//->Unit(benchmark::kMillisecond);
 
 
 void BM_VectorUpdate(benchmark::State& state) {
     std::vector <std::pair <double, int>> vec;
+    std::ifstream ifs("test");
+    double ar_price[ar_size];
+    int ar_amount[ar_size];
+    for(int i=0; i<ar_size; i++){
+       ifs >> ar_price[i];
+       ifs >> ar_amount[i];
+       if(!ifs.good()) {
+           std::cerr<<"\nERROR 1\n";
+           exit(1);
+       }
+    }
+    ifs.close();
+
     for(auto _ : state){
-        std::ifstream ifs("test");
-        updateVector(ifs, vec);
-        ifs.close();
+        updateVector(ar_price, ar_amount, vec);
     }
 }
-BENCHMARK(BM_VectorUpdate)->Unit(benchmark::kMillisecond);
+BENCHMARK(BM_VectorUpdate);//->Unit(benchmark::kMillisecond);
 
 void BM_ListUpdate(benchmark::State& state) {
     std::list <std::pair <double, int>> l;
+    std::ifstream ifs("test");
+    double ar_price[ar_size];
+    int ar_amount[ar_size];
+    for(int i=0; i<ar_size; i++){
+       ifs >> ar_price[i];
+       ifs >> ar_amount[i];
+       if(!ifs.good()) {
+           std::cerr<<"\nERROR 1\n";
+           exit(1);
+       }
+    }
+    ifs.close();
+
     for(auto _ : state){
-        std::ifstream ifs("test");
-        updateList(ifs, l);
-        ifs.close();
+        updateList(ar_price, ar_amount, l);
     }
 }
-BENCHMARK(BM_ListUpdate)->Unit(benchmark::kMillisecond);
+BENCHMARK(BM_ListUpdate);//->Unit(benchmark::kMillisecond);
 
 
 BENCHMARK_MAIN();
